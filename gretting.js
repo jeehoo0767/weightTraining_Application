0884c5fea627cb3368d311f77c6c2479b19894da
alsruddl0805 <66991772+jeehoo0767@users.noreply.github.com>
2020-09-29 16:50:14 +0900

    SAVE

--- gretting.js
+++ gretting.js
@@ -24,9 +24,12 @@ function askForName() {
 function paintGreeting(text) {
     form.classList.remove(SHOWING_CN);
     greeting.classList.add(SHOWING_CN);
-    greeting.innerHTML = `안녕하세요 ${text}님 본 웹사이트는<br> 웨이트 트레이닝을 위한
+    greeting.innerHTML = `<div class="flex_box"><div class="hangule_box show">안녕하세요 ${text}님 본 웹사이트는<br> 웨이트 트레이닝을 위한
     정보와 <br>트레이닝 스케쥴 관리를 위한 사이트 입니다.<br>
-    아래로 내려 가시면 많은 컨텐츠를<br> 이용 하실 수 있습니다.`;
+    아래로 내려 가시면 많은 컨텐츠를<br> 이용 하실 수 있습니다.</div>
+    <div class="eng_box hide">Hello, ${text} This website is for weight training.<br>
+    This site is for information of weight training<br> and training schedule management.<br> If you go down, you can use a lot of contents.<div>
+    </div>`;
     greeting.classList.add("font-size-big");
 }
 
