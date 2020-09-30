64c9db4a2d590c9e813057918b81231de68f5622
jeehoo0767 <jeehoo0767@naver.com>
2020-10-01 06:31:15 +0900

    save

--- gretting.js
+++ gretting.js
@@ -24,12 +24,11 @@ function askForName() {
 function paintGreeting(text) {
     form.classList.remove(SHOWING_CN);
     greeting.classList.add(SHOWING_CN);
-    greeting.innerHTML = `<div class="flex_box"><div class="hangule_box show">안녕하세요 ${text}님 본 웹사이트는<br> 웨이트 트레이닝을 위한
-    정보와 <br>트레이닝 스케쥴 관리를 위한 사이트 입니다.<br>
-    아래로 내려 가시면 많은 컨텐츠를<br> 이용 하실 수 있습니다.</div>
+    greeting.innerHTML = `<div class="hangule_box show"><span class="big_font">안녕하세요 ${text}님,<br> 본 웹사이트는 웨이트 트레이닝을 위한
+    정보 습득과<br>트레이닝 스케쥴 관리를 위한 사이트 입니다</span>.<br>
+    <span class="small_font">많은 컨텐츠를 이용 해 보세요</span>.</div>
     <div class="eng_box hide">Hello, ${text} This website is for weight training.<br>
-    This website contains information for weight training and training schedule management.<br> If you go down, you can use a lot of contents.<div>
-    </div>`;
+    This website contains information for weight training and training schedule management.<br> Use a lot of content..<div>`;
     greeting.classList.add("font-size-big");
 }
 
