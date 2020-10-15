aa1068b8f481c3c678658099e20f6fff4d7e7bb9
alsruddl0805 <66991772+jeehoo0767@users.noreply.github.com>
2020-10-15 16:42:26 +0900

    indexjsMethodChange

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ $(".menu_container").click(function () {
     $("#line-top").toggleClass("bar-top").toggleClass("bar-top-reverse");
     $("#line-middle").toggleClass("bar-middle").toggleClass("bar-middle-reverse");
     $("#line-bottom").toggleClass("bar-bottom").toggleClass("bar-bottom-reverse");
-    $(".gnb_toggle").toggleClass("fadeout");
+    $(".gnb_toggle").fadeToggle(200);
 });
