cfed7843405abf0d19dbb6a2cb2a83df75efcecb
alsruddl0805 <66991772+jeehoo0767@users.noreply.github.com>
2020-10-15 16:26:00 +0900

    파일분리

---
+++ index.js
@@ -0,0 +1,6 @@
+$(".menu_container").click(function () {
+    $("#line-top").toggleClass("bar-top").toggleClass("bar-top-reverse");
+    $("#line-middle").toggleClass("bar-middle").toggleClass("bar-middle-reverse");
+    $("#line-bottom").toggleClass("bar-bottom").toggleClass("bar-bottom-reverse");
+    $(".gnb_toggle").toggleClass("fadeout");
+});
